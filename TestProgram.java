9b06d7ffc94b31ba0fbf02f13be6f657984054b0
mdhumayun11 <mdhumayun53@gmail.com>
2022-09-13 00:00:15 +0530

    Test commit

--- TestProgram.java
+++ TestProgram.java
@@ -5,10 +5,9 @@ public class TestProgram{
   public static void main(String args[]){ 
  Scanner sc= new Scanner(System.in);
  String name = sc.nextLine();
-int age = sc.nextInt();
+ int age = sc.nextInt();
 System.out.println("Name: "+name);
 System.out.println("Age: "+age);
- 
   } 
 }  
 
