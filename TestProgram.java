dd09bcfff69a34455f17bc0fbecd95f54090e13a
mdhumayun11 <mdhumayun53@gmail.com>
2022-09-12 17:29:21 +0530

    test commit

---
+++ TestProgram.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class TestProgram{    
+  public static void main(String args[]){ 
+ Scanner sc= new Scanner(System.in);
+ String name = sc.nextLine();
+int age = sc.nextInt();
+System.out.println("Name: "+name);
+System.out.println("Age: "+age);
+ 
+  } 
+}  
+
+     
