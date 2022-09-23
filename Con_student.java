5b7177f70778cb4e1718979534835be1d97735a2
mdhumayun11 <mdhumayun53@gmail.com>
2022-09-23 07:17:11 +0530

    commit

--- Con_student.java
+++ Con_student.java
@@ -1,4 +1,12 @@
-
 public class Con_student {
-
+   public Con_student()
+	{
+		System.out.println("Student object is created ");
+		
+	}
+public static void main(String[] args)
+{
+	Con_student obj =new Con_student();
+	
+}
 }
